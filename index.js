7da2b93422f37d4cdac69621b0fc12e79f317546
Josey Morton <me@joseymorton.com>
2015-04-22 14:23:46 -0500

    Allowing for reference fields
    Adding reference fields to allow for multiple counters for the same model.
    
    For example, a user can have many comments, but each comment should increment from 0 per user. You could reference the _id of the User model making it so that theres a counter for each user.

--- index.js
+++ index.js
@@ -14,11 +14,13 @@ exports.initialize = function (connection) {
       counterSchema = new mongoose.Schema({
         model: { type: String, require: true },
         field: { type: String, require: true },
-        count: { type: Number, default: 0 }
+        count: { type: Number, default: 0 },
+        reference_field: { type: String, require: false, default: null },
+        reference: { type: mongoose.Schema.Types.Mixed, default: null }
       });
 
       // Create a unique index using the "field" and "model" fields.
-      counterSchema.index({ field: 1, model: 1 }, { unique: true, required: true, index: -1 });
+      counterSchema.index({ field: 1, model: 1, reference_field: 1, reference: 1 }, { unique: true, required: true, index: -1 });
 
       // Create model using new schema.
       IdentityCounter = connection.model('IdentityCounter', counterSchema);
@@ -69,23 +71,6 @@ exports.plugin = function (schema, options) {
     fields[settings.field].unique = settings.unique
   schema.add(fields);
 
-  // Find the counter for this model and the relevant field.
-  IdentityCounter.findOne(
-    { model: settings.model, field: settings.field },
-    function (err, counter) {
-      if (!counter) {
-        // If no counter exists then create one and save it.
-        counter = new IdentityCounter({ model: settings.model, field: settings.field, count: settings.startAt - settings.incrementBy });
-        counter.save(function () {
-          ready = true;
-        });
-      }
-      else {
-        ready = true;
-      }
-    }
-  );
-
   // Declare a function to get the next counter for the model/schema.
   var nextCount = function (callback) {
     IdentityCounter.findOne({
@@ -123,32 +108,73 @@ exports.plugin = function (schema, options) {
 
     // Only do this if it is a new document (see http://mongoosejs.com/docs/api.html#document_Document-isNew)
     if (doc.isNew) {
+
+      var query = { model: settings.model, field: settings.field };
+      if(typeof settings.reference_field !== 'undefined' && settings.reference_field) {
+        query['reference_field'] = settings.reference_field;
+        query['reference'] = doc[settings.reference_field];
+      }
+      // Find the counter for this model and the relevant field.
+      IdentityCounter.findOne(
+        query,
+        function (err, counter) {
+          if (!counter) {
+            // If no counter exists then create one and save it.
+            var data = { model: settings.model, field: settings.field, count: settings.startAt - settings.incrementBy };
+            if(typeof settings.reference_field !== 'undefined' && settings.reference_field){
+              data.reference_field = settings.reference_field;
+              data.reference = doc[settings.reference_field];
+            }
+            counter = new IdentityCounter(data);
+            counter.save(function (err) {
+              console.error(err);
+              if(!err) {
+                ready = true;
+                save();
+              }
+            });
+          }
+          else {
+            ready = true;
+            save();
+          }
+        }
+      );
+
       // Declare self-invoking save function.
-      (function save() {
+      function save() {
         // If ready, run increment logic.
         // Note: ready is true when an existing counter collection is found or after it is created for the
         // first time.
         if (ready) {
           // check that a number has already been provided, and update the counter to that number if it is
           // greater than the current count
+
           if (typeof doc[settings.field] === 'number') {
+            
+            query['count'] = { $lt: doc[settings.field] };
+
             IdentityCounter.findOneAndUpdate(
               // IdentityCounter documents are identified by the model and field that the plugin was invoked for.
               // Check also that count is less than field value.
-              { model: settings.model, field: settings.field, count: { $lt: doc[settings.field] } },
+              query,
               // Change the count of the value found to the new field value.
               { count: doc[settings.field] },
-              function (err) {
+              function (err, counter) {
                 if (err) return next(err);
                 // Continue with default document save functionality.
                 next();
               }
             );
+
           } else {
+
+            console.log(query);
+
             // Find the counter collection entry for this model and field and update it.
             IdentityCounter.findOneAndUpdate(
               // IdentityCounter documents are identified by the model and field that the plugin was invoked for.
-              { model: settings.model, field: settings.field },
+              query,
               // Increment the count by `incrementBy`.
               { $inc: { count: settings.incrementBy } },
               // new:true specifies that the callback should get the counter AFTER it is updated (incremented).
@@ -168,7 +194,7 @@ exports.plugin = function (schema, options) {
         // the counter collection is ready.
         else
           setTimeout(save, 5);
-      })();
+      };
     }
     // If the document does not have the field we're interested in or that field isn't a number AND the user did
     // not specify that we should increment on updates, then just continue the save without any increment logic.
