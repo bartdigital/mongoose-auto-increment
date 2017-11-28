c44d79bcd678d07d69dfee4cc5a65e3a1e733fdb
marcelobart <31290847+marcelobart@users.noreply.github.com>
2017-11-28 14:16:12 -0200

    Add hook and hookValidation params

--- index.js
+++ index.js
@@ -39,6 +39,8 @@ exports.plugin = function (schema, options) {
 
   // Default settings and plugin scope variables.
   var settings = {
+    hook: 'save',
+    hookValidation: false,
     model: null, // The model to configure the plugin for.
     field: '_id', // The field the plugin should track.
     startAt: 0, // The number the count should start at.
@@ -107,7 +109,7 @@ exports.plugin = function (schema, options) {
   schema.static('resetCount', resetCount);
 
   // Every time documents in this schema are saved, run this logic.
-  schema.pre('save', function (next) {
+  schema.pre(settings.hook, function (next) {
     // Get reference to the document being saved.
     var doc = this;
 
@@ -116,86 +118,88 @@ exports.plugin = function (schema, options) {
       query['reference'] = doc[settings.referenceField];
     }
 
-    // Only do this if it is a new document (see http://mongoosejs.com/docs/api.html#document_Document-isNew)
-    if (doc.isNew) {
+    if(!settings.hookValidation || (settings.hookValidation && settings.hookValidation(doc))){
+      // Only do this if it is a new document (see http://mongoosejs.com/docs/api.html#document_Document-isNew)
+      if (doc.isNew) {
 
-      // Find the counter for this model and the relevant field.
-      IdentityCounter.findOne(
-        query,
-        function (err, counter) {
-          if (!counter) {
-            // If no counter exists then create one and save it.
-            var data = { model: settings.model, field: settings.field, count: settings.startAt - settings.incrementBy };
-            if(typeof settings.referenceField !== 'undefined' && settings.referenceField){
-              data.referenceField = settings.referenceField;
-              data.reference = doc[settings.referenceField];
+        // Find the counter for this model and the relevant field.
+        IdentityCounter.findOne(
+          query,
+          function (err, counter) {
+            if (!counter) {
+              // If no counter exists then create one and save it.
+              var data = { model: settings.model, field: settings.field, count: settings.startAt - settings.incrementBy };
+              if(typeof settings.referenceField !== 'undefined' && settings.referenceField){
+                data.referenceField = settings.referenceField;
+                data.reference = doc[settings.referenceField];
+              }
+              counter = new IdentityCounter(data);
+              counter.save(function (err, blah) {
+                if(!err) {
+                  ready = true;
+                  save();
+                }
+              });
             }
-            counter = new IdentityCounter(data);
-            counter.save(function (err, blah) {
-              if(!err) {
-                ready = true;
-                save();
-              }
-            });
+            else {
+              ready = true;
+              save();
+            }
           }
-          else {
-            ready = true;
-            save();
+        );
+
+        // Declare self-invoking save function.
+        function save() {
+          // If ready, run increment logic.
+          // Note: ready is true when an existing counter collection is found or after it is created for the
+          // first time.
+          if (ready) {
+            // check that a number has already been provided, and update the counter to that number if it is
+            // greater than the current count
+
+            if (typeof doc[settings.field] === 'number') {
+
+              query['count'] = { $lt: doc[settings.field] };
+
+              IdentityCounter.findOneAndUpdate(
+                // IdentityCounter documents are identified by the model and field that the plugin was invoked for.
+                // Check also that count is less than field value.
+                query,
+                // Change the count of the value found to the new field value.
+                { count: doc[settings.field] },
+                function (err, counter) {
+                  if (err) return next(err);
+                  // Continue with default document save functionality.
+                  next();
+                }
+              );
+
+            } else {
+              // Find the counter collection entry for this model and field and update it.
+              IdentityCounter.findOneAndUpdate(
+                // IdentityCounter documents are identified by the model and field that the plugin was invoked for.
+                query,
+                // Increment the count by `incrementBy`.
+                { $inc: { count: settings.incrementBy } },
+                // new:true specifies that the callback should get the counter AFTER it is updated (incremented).
+                { new: true },
+                // Receive the updated counter.
+                function (err, updatedIdentityCounter) {
+                  if (err) return next(err);
+                  // If there are no errors then go ahead and set the document's field to the current count.
+                  doc[settings.field] = updatedIdentityCounter.count;
+                  // Continue with default document save functionality.
+                  next();
+                }
+              );
+            }
           }
-        }
-      );
-
-      // Declare self-invoking save function.
-      function save() {
-        // If ready, run increment logic.
-        // Note: ready is true when an existing counter collection is found or after it is created for the
-        // first time.
-        if (ready) {
-          // check that a number has already been provided, and update the counter to that number if it is
-          // greater than the current count
-
-          if (typeof doc[settings.field] === 'number') {
-            
-            query['count'] = { $lt: doc[settings.field] };
-
-            IdentityCounter.findOneAndUpdate(
-              // IdentityCounter documents are identified by the model and field that the plugin was invoked for.
-              // Check also that count is less than field value.
-              query,
-              // Change the count of the value found to the new field value.
-              { count: doc[settings.field] },
-              function (err, counter) {
-                if (err) return next(err);
-                // Continue with default document save functionality.
-                next();
-              }
-            );
-
-          } else {
-            // Find the counter collection entry for this model and field and update it.
-            IdentityCounter.findOneAndUpdate(
-              // IdentityCounter documents are identified by the model and field that the plugin was invoked for.
-              query,
-              // Increment the count by `incrementBy`.
-              { $inc: { count: settings.incrementBy } },
-              // new:true specifies that the callback should get the counter AFTER it is updated (incremented).
-              { new: true },
-              // Receive the updated counter.
-              function (err, updatedIdentityCounter) {
-                if (err) return next(err);
-                // If there are no errors then go ahead and set the document's field to the current count.
-                doc[settings.field] = updatedIdentityCounter.count;
-                // Continue with default document save functionality.
-                next();
-              }
-            );
-          }
-        }
-        // If not ready then set a 5 millisecond timer and try to save again. It will keep doing this until
-        // the counter collection is ready.
-        else
-          setTimeout(save, 5);
-      };
+          // If not ready then set a 5 millisecond timer and try to save again. It will keep doing this until
+          // the counter collection is ready.
+          else
+            setTimeout(save, 5);
+        };
+      }
     }
     // If the document does not have the field we're interested in or that field isn't a number AND the user did
     // not specify that we should increment on updates, then just continue the save without any increment logic.
